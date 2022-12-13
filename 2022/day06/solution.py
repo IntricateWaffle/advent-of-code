53385475a7f3859480d85f8bc37ef643d6358fa4
IntricateWaffle <IntricateWaffle@gmail.com>
2022-12-12 20:35:31 -0600

    add day 06 part 2 solution

--- 2022/day06/solution.py
+++ 2022/day06/solution.py
@@ -4,13 +4,13 @@ from pathlib import Path
 # puzzle_input = Path("test.txt ")
 puzzle_input = Path("input.txt")
 
-def get_sop_marker(input_line):
+def get_sop_marker(input_line, length):
     print(f"{input_line=}")
     for i in range(len(input_line)):
-        possible_sop_marker = set(input_line[i:i+4])
+        possible_sop_marker = set(input_line[i:i+length])
         print(f"{possible_sop_marker=}")
-        if len(possible_sop_marker) == 4:
-            print(f"{i+4=}")
+        if len(possible_sop_marker) == length:
+            print(f"{i+length=}")
             break
         
 
@@ -19,12 +19,19 @@ def get_sop_marker(input_line):
 
 def part1(input_lines):
     for line in input_lines:
-        get_sop_marker(line)
+        get_sop_marker(line,4)
     return
+
+def part2(input_lines):
+    for line in input_lines:
+        get_sop_marker(line,14)
+    return
+
 
 def main():
     with open(puzzle_input, "r") as f:
         lines = [line.strip() for line in f]
         print(f"{lines=}")
     part1(lines)
+    part2(lines)
 main()
